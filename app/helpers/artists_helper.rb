5a83a257da9df764e820e535cda3d57769710ac7
nabilhayet <nabil4457@yahoo.com>
2020-08-17 21:17:27 -0400

    Done.

--- app/helpers/artists_helper.rb
+++ app/helpers/artists_helper.rb
@@ -1,2 +1,10 @@
 module ArtistsHelper
+
+    def display_artist(song)
+        if self.songs.include?(song)
+            link_to "Artist Show", artist_path(self)
+        else
+            link_to "Add Artist", edit_song_path(song)
+        end 
+    end 
 end
